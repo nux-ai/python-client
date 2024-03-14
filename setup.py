f7bbf002e5679017c56d70629c5975dbfe0e3200
Ethan Steininger <esteininger21@gmail.com>
2024-03-14 15:48:12 -0400

    no message

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-
 from setuptools import setup, find_packages
 
 
 setup(
-    name='nuxai',
-    version='0.2',
-    license='MIT',
+    name="nuxai",
+    version="0.2",
+    license="MIT",
     author="Ethan Steininger",
-    author_email='ethan@nux.ai',
-    packages=['nuxai'],
-    url='https://github.com/nux-ai/nux-python',
-    keywords='LLM Pipelines',
-    description='llm pipelines',
-    install_requires=['requests']
+    author_email="ethan@nux.ai",
+    packages=["nuxai"],
+    url="https://github.com/nux-ai/python-client",
+    keywords="LLM Pipelines",
+    description="llm pipelines",
+    install_requires=["requests"],
 )
